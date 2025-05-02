5871f9c029c0f6b98e4a9935c63b17f76eca884b
Mrxia <2452588468@qq.com>
2025-05-02 15:56:17 +0800

    Update proxy-handler.ts

--- netlify/edge-functions/proxy-handler.ts
+++ netlify/edge-functions/proxy-handler.ts
@@ -23,7 +23,7 @@ const PROXY_CONFIG = {
   // 任意网址
   "/hexo": "https://hexo-gally.vercel.app", 
   "/hexo2": "https://hexo-987.pages.dev",
-  "/halo": "https://blog.gally.dpdns.org",
+  "/halo": "https://www.dzkji.cn",
   "/kuma": "https://status.dzkji.cn",
   "/hf": "https://huggingface.co",
   "/tv": "https://tv.gally.ddns-ip.net",
